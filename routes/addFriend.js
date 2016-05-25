6592768555e0f2860a23c3830c5208d46ce6929d
Park-Hyeji <ehddn159@naver.com>
2016-05-26 04:39:58 +0900

    a가 b친구 추가 하면 b친구 목록에 a가 등시에 등록되는 친구 추가 버전 + 친구 리스트 select 완료

--- routes/addFriend.js
+++ routes/addFriend.js
@@ -17,28 +17,33 @@ router.get('/', function(req, res) {
 
 router.post('/', function(req,res,next){
 	pool.getConnection(function(err,connection){
-		console.log("DB CONNECT");
+		console.log("addFriend DB CONNECT");
 		connection.query('select count(*) cnt from customer where c_id = ?',req.body.friendIdAdd,function(err,rows){
 			var cnt = rows[0].cnt;
 			if(cnt == 0){
-				res.send('<script>alert("아이디가 없습니다.");history.back();</script>');
+				res.send('<script>alert("아이디가 없습니다."); history.back();</script>');
 			}else{
-				connection.query('select count(*) cnt2 from friend where f_id = ?',req.body.friendIdAdd,function(err,rows2){
+				var data = [req.body.c_id,req.body.friendIdAdd];
+				connection.query('select count(*) cnt2 from friend where c_id = ? and f_id = ?',data,function(err,rows2){
 					var cnt2 = rows2[0].cnt2;
 					if(cnt2 == 1){
 						res.send('<script>alert("이미 등록했습니다."); history.back();</script>');
 					}else{
-						var data = [req.body.c_id,req.body.friendIdAdd];
+						var data2 = [req.body.friendIdAdd,req.body.c_id];
 						connection.query('insert into friend values(?,?)',data, function(err,rows3){
 							if(err) console.err('err', err);
 							console.log('rows3',rows3);
-							res.send("<script>alert('친구를 추가했습니다.');history.back();</script>");
 						});
+						connection.query('insert into friend values(?,?)',data2, function(err,rows4){
+							if(err) console.err('err', err);
+							console.log('rows4',rows4);
+						});
+						res.send("<script>alert('친구를 추가했습니다.');history.back();</script>");
 					}
 				});
 			}
 		});		
-		connection.release();
+		connection.release();	
 	});
 });
 
--- routes/chatList.js
+++ routes/chatList.js
@@ -15,10 +15,14 @@ router.get('/',function(req, res){
 	var id = req.query.id;
 	pool.getConnection(function(err,connection){
 		console.log("chatList DB CONNECT");
-		connection.query('select count(*) cnt, c.c_id, c.c_img, c.c_name, c.c_msg, f.c_id fc_id, f.f_id from customer_info c left join friend f on c.c_id = f.c_id where c.c_id=?',id, function(err,rows){
+		connection.query('select * from customer_info where c_id=?',id,function(err,rows){
 			if(err) console.err('err', err);
 			console.log('rows',rows);
-			res.render('chatList',{rows:rows});
+			connection.query('select c.c_id, c.c_img, c.c_name, c.c_msg, f.c_id fc_id, f.f_id from customer_info c left join friend f on c.c_id = f.c_id where f.f_id = ?',id, function(err,rows2){
+				if(err) console.err('err', err);
+				console.log('rows2',rows2);
+				res.render('chatList',{rows:rows, rows2:rows2});
+			});
 		});
 		connection.release();
 	});
