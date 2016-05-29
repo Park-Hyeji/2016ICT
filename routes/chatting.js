77520e3ba52373f9a05021426dca76bb9fea10ba
Park-Hyeji <ehddn159@naver.com>
2016-05-29 15:48:00 +0900

    데이터 베이스 두 번째 친구부터 친구 안 되던거 디비 바꾸고 코드 바꿔서 제대로 돌아가게 함

--- routes/chatting.js
+++ routes/chatting.js
@@ -9,6 +9,8 @@ var pool = mysql.createPool({
 	database: 'ict',
 	password: 'softwareproject'
 });
+var realChatId;
+var rrealChatId;
 
 /* GET home page. */
 router.get('/', function(req, res){	
@@ -56,31 +58,42 @@ router.get('/', function(req, res){
 						*/
 					}else{
 						//c_id 방 있음
+						var sql3 = 'select * from chat_room where c_id = "' + room + '" and chat_id IN ('
+						var tempArray = new Array();
 						for(var i=0; i<rows.length; i++){
-							var sql3 = 'select * from chat_room where chat_id = "' + rows[i].chat_id + '" and c_id = "'+ room + '"';
-							connection.query(sql3,function(err,rowss){
-								if(err) console.err('err', err);
-								console.log('rowss',rowss);
-								if(rowss.length !== 0){
-									var sql2 = 'select * from chat_msg where chat_id = "' + rowss[0].chat_id + '"';
-									connection.query(sql2,function(err,rows2){
-										if(err) console.err('err',err);
-										console.log('rows2',rows2);
-										res.render('chatting',{rowss:rowss, row:row, c_row:c_row, rows2:rows2});
-									});
-								}else{
-									var data = [id,room];
-									var newSql = 'insert into chat_room (chat_id, chat_name, c_id) values("'+roomName+'","'+roomName+'","'+id+'"),("'+roomName+'","'+roomName+'","'+id+'")';
-									connection.query(newSql,function(err,newRows){
-										console.log("newSql",newSql);
-										if(err) console.err('err',err);	
-										var rows2 = [];
-										var rowss = [];
-										res.render('chatting',{rowss:rowss, row:row, c_row:c_row, rows2:rows2});								
-									});	
-								}
-							});
+							tempArray[i] = connection.escape(rows[i].chat_id);
+							if(i == rows.length-1){
+								sql3 += tempArray[i];
+							}else{
+								sql3 += tempArray[i] + ",";
+							}
 						}
+						sql3 += ')';
+						console.log(sql3);
+						connection.query(sql3, function(err,rowss){
+							if(err) console.err('err', err);
+							console.log('rowss',rowss);		
+							realChatId = rowss[0].chat_id;
+									
+							if(realChatId !== 0){
+								var sql2 = 'select * from chat_msg where chat_id = "' + realChatId + '"';
+								connection.query(sql2,function(err,rows2){
+									if(err) console.err('err',err);
+									console.log('rows2',rows2);
+									res.render('chatting',{rowss:rowss, row:row, c_row:c_row, rows2:rows2});
+								});
+							}else{
+								var data = [id,room];
+								var newSql = 'insert into chat_room (chat_id, chat_name, c_id) values("'+roomName+'","'+roomName+'","'+id+'"),("'+roomName+'","'+roomName+'","'+room+'")';
+								connection.query(newSql,function(err,newRows){
+									console.log("newSql",newSql);
+									if(err) console.err('err',err);	
+									var rows2 = [];
+									var rowss = [];
+									res.render('chatting',{rowss:rowss, row:row, c_row:c_row, rows2:rows2});								
+								});	
+							}	
+						});							
 					}
 				});
 			});
